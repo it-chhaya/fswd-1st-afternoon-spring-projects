b855ae07f9ab787d264e660e629faa7fa16c7179
techmentors2024 <techmentors2024@gmail.com>
2025-07-31 15:58:17 +0700

    integrate with keycloak

---
+++ aft-mobile-banking-api/src/main/java/kh/edu/cstad/mbapi/security/KeycloakSecurityConfig.java
@@ -0,0 +1,81 @@
+package kh.edu.cstad.mbapi.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class KeycloakSecurityConfig {
+
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        Converter<Jwt, Collection<GrantedAuthority>> converter = jwt -> {
+            Map<String, Collection<String>> realmAccess = jwt.getClaim("realm_access");
+            Collection<String> roles = realmAccess.get("roles");
+            return roles.stream()
+                    .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                    .collect(Collectors.toList());
+        };
+        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
+        jwtConverter.setJwtGrantedAuthoritiesConverter(converter);
+
+        return jwtConverter;
+    }
+
+
+    @Bean
+    public SecurityFilterChain apiSecurity(HttpSecurity http) throws Exception {
+        // TODO
+        // All requests must be authenticated
+        http.authorizeHttpRequests(request
+                -> request
+                .requestMatchers(HttpMethod.POST, "/api/v1/customers/**")
+                .hasAnyRole("ADMIN", "STAFF")
+                .requestMatchers(HttpMethod.PUT, "/api/v1/customers/**")
+                .hasAnyRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/api/v1/customers/**")
+                .hasAnyRole("ADMIN")
+                .requestMatchers(HttpMethod.GET, "/api/v1/customers/**")
+                .hasAnyRole("ADMIN", "STAFF", "CUSTOMER")
+                .requestMatchers("/api/v1/accounts/**")
+                .hasRole("USER")
+                .anyRequest().authenticated()
+        );
+
+        // Disable form login default
+        http.formLogin(form -> form.disable());
+        http.csrf(token -> token.disable());
+
+        // Set security mechanism
+        // Basic Authentication (username & password)
+        http.oauth2ResourceServer(oauth2 ->
+                oauth2.jwt(Customizer.withDefaults())
+        );
+
+        // Set session to stateless
+        http.sessionManagement(session
+                -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+
+        return http.build();
+    }
+
+}
--- aft-mobile-banking-api/src/main/java/kh/edu/cstad/mbapi/security/SecurityConfig.java
+++ aft-mobile-banking-api/src/main/java/kh/edu/cstad/mbapi/security/SecurityConfig.java
@@ -13,86 +13,86 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity
-@RequiredArgsConstructor
-public class SecurityConfig {
-
+//@Configuration
+//@EnableWebSecurity
+//@RequiredArgsConstructor
+//public class SecurityConfig {
+//
+////    @Bean
+////    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+////        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+////
+////        // Create ADMIN
+////        UserDetails admin = User.builder()
+////                .username("admin")
+////                .password("{noop}admin@123")
+////                .roles("ADMIN")
+////                .build();
+////        manager.createUser(admin);
+////
+////        // Create STAFF
+////        UserDetails staff = User.builder()
+////                .username("staff")
+////                .password("{noop}staff@123")
+////                .roles("STAFF")
+////                .build();
+////        manager.createUser(staff);
+////
+////        // Create CUSTOMER
+////        UserDetails customer = User.builder()
+////                .username("customer")
+////                .password("{noop}customer@123")
+////                .roles("CUSTOMER")
+////                .build();
+////        manager.createUser(customer);
+////
+////        return manager;
+////    }
+//
+//    private final PasswordEncoder passwordEncoder;
+//    private final UserDetailsService userDetailsService;
+//
 //    @Bean
-//    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-//
-//        // Create ADMIN
-//        UserDetails admin = User.builder()
-//                .username("admin")
-//                .password("{noop}admin@123")
-//                .roles("ADMIN")
-//                .build();
-//        manager.createUser(admin);
-//
-//        // Create STAFF
-//        UserDetails staff = User.builder()
-//                .username("staff")
-//                .password("{noop}staff@123")
-//                .roles("STAFF")
-//                .build();
-//        manager.createUser(staff);
-//
-//        // Create CUSTOMER
-//        UserDetails customer = User.builder()
-//                .username("customer")
-//                .password("{noop}customer@123")
-//                .roles("CUSTOMER")
-//                .build();
-//        manager.createUser(customer);
-//
-//        return manager;
+//    public DaoAuthenticationProvider daoAuthProvider() {
+//        DaoAuthenticationProvider daoAuthProvider = new DaoAuthenticationProvider(userDetailsService);
+//        daoAuthProvider.setPasswordEncoder(passwordEncoder);
+//        return daoAuthProvider;
 //    }
-
-    private final PasswordEncoder passwordEncoder;
-    private final UserDetailsService userDetailsService;
-
-    @Bean
-    public DaoAuthenticationProvider daoAuthProvider() {
-        DaoAuthenticationProvider daoAuthProvider = new DaoAuthenticationProvider(userDetailsService);
-        daoAuthProvider.setPasswordEncoder(passwordEncoder);
-        return daoAuthProvider;
-    }
-
-
-    @Bean
-    public SecurityFilterChain apiSecurity(HttpSecurity http) throws Exception {
-        // TODO
-        // All requests must be authenticated
-        http.authorizeHttpRequests(request
-                -> request
-                .requestMatchers(HttpMethod.POST, "/api/v1/customers/**")
-                .hasAnyRole("ADMIN", "STAFF")
-                .requestMatchers(HttpMethod.PUT, "/api/v1/customers/**")
-                .hasAnyRole("ADMIN")
-                .requestMatchers(HttpMethod.DELETE, "/api/v1/customers/**")
-                .hasAnyRole("ADMIN")
-                .requestMatchers(HttpMethod.GET, "/api/v1/customers/**")
-                .hasAnyRole("ADMIN", "STAFF", "CUSTOMER")
-                .requestMatchers("/api/v1/accounts/**")
-                .hasRole("USER")
-                .anyRequest().authenticated()
-        );
-
-        // Disable form login default
-        http.formLogin(form -> form.disable());
-        http.csrf(token -> token.disable());
-
-        // Set security mechanism
-        // Basic Authentication (username & password)
-        http.httpBasic(Customizer.withDefaults());
-
-        // Set session to stateless
-        http.sessionManagement(session
-                -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        );
-
-        return http.build();
-    }
-
-}
+//
+//
+//    @Bean
+//    public SecurityFilterChain apiSecurity(HttpSecurity http) throws Exception {
+//        // TODO
+//        // All requests must be authenticated
+//        http.authorizeHttpRequests(request
+//                -> request
+//                .requestMatchers(HttpMethod.POST, "/api/v1/customers/**")
+//                .hasAnyRole("ADMIN", "STAFF")
+//                .requestMatchers(HttpMethod.PUT, "/api/v1/customers/**")
+//                .hasAnyRole("ADMIN")
+//                .requestMatchers(HttpMethod.DELETE, "/api/v1/customers/**")
+//                .hasAnyRole("ADMIN")
+//                .requestMatchers(HttpMethod.GET, "/api/v1/customers/**")
+//                .hasAnyRole("ADMIN", "STAFF", "CUSTOMER")
+//                .requestMatchers("/api/v1/accounts/**")
+//                .hasRole("USER")
+//                .anyRequest().authenticated()
+//        );
+//
+//        // Disable form login default
+//        http.formLogin(form -> form.disable());
+//        http.csrf(token -> token.disable());
+//
+//        // Set security mechanism
+//        // Basic Authentication (username & password)
+//        http.httpBasic(Customizer.withDefaults());
+//
+//        // Set session to stateless
+//        http.sessionManagement(session
+//                -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//        );
+//
+//        return http.build();
+//    }
+//
+//}
